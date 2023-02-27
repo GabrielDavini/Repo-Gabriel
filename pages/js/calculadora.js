46b24e98ce022f33616007de05f66bad2ab47dbd
Gabriel Davini <gabriel.davini@comafi.com.ar>
2023-02-27 18:53:28 -0300

    test

---
+++ pages/js/calculadora.js
@@ -0,0 +1,21 @@
+//escribir un programa que sume numeros
+//y lo muestra por pantalla
+let a;
+let b;
+let c;
+
+a = 2;
+//averiguar que tipo de variable
+//typeof
+console.log(typeof(a));
+b = 2;
+console.log(typeof(b));
+c = a + b;
+console.log(typeof(c));
+//fin
+a = 'carlos';
+console.log('a, b, c', typeof(a),typeof(b),typeof(c));
+b = 'lopez';
+c = a + b;
+console.log(c);
+//????
---
+++ pages/js/variables.js
@@ -0,0 +1,13 @@
+/*variables*/
+/*numeros*/
+let edad = 36;
+/*muestro la variable*/
+console console.log(edad);
+
+/*cambio el valor*/
+edad = 45;
+alert(edad);
+
+/*constante (cadena,string) entre ' ' o " " */
+const dni=12345678;
+//dni = '987654321'; no se puede cambiar!
